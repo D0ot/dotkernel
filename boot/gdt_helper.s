41cc9e368b8a17bdaf53a22b8baf099bbe816dbc
D0ot <1306793135@qq.com>
2020-04-23 01:43:01 +0800

    temp commit

---
+++ boot/gdt_helper.s
@@ -0,0 +1,48 @@
+; make my life easy
+
+
+; constants used as rhs operands of "<<" operator
+DESC_BASE_OFFSET1       equ         16          ; length 16
+DESC_BASE_OFFSET2       equ         0           ; length 8
+DESC_BASE_OFFSET3       equ         24          ; length 8
+DESC_AVL_OFFSET         equ         20
+
+
+; constants used as operands of "||" operator
+DESC_TYPE_DATA_RD       equ         0x00 << 0x08 ;  Read-Only
+DESC_TYPE_DATA_RDA      equ         0x01 << 0x08 ;  Read-Only, accessed
+DESC_TYPE_DATA_RDWR     equ         0x02 << 0x08 ;  Read/Write
+DESC_TYPE_DATA_RDWRA    equ         0x03 << 0x08 ;  Read/Write, accessed
+DESC_TYPE_DATA_RDEXPD   equ         0x04 << 0x08 ;  Read-Only, expand-down
+DESC_TYPE_DATA_RDEXPDA  equ         0x05 << 0x08 ;  Read-Only, expand-down, accessed
+DESC_TYPE_DATA_RDWREXPD equ         0x06 << 0x08 ;  Read/Write, expand-down
+DESC_TYPE_DATA_RDWREXPDA equ        0x07 << 0x08 ;  Read/Write, expand-down, accessed
+DESC_TYPE_CODE_EX       equ         0x08 << 0x08 ;  Execute-OnlyPE
+DESC_TYPE_CODE_EXA      equ         0x09 << 0x08 ;  Execute-Only, accessed
+DESC_TYPE_CODE_EXRD     equ         0x0A << 0x08 ;  Execute/Read
+DESC_TYPE_CODE_EXRDA    equ         0x0B << 0x08 ;  Execute/Read, accessed
+DESC_TYPE_CODE_EXC      equ         0x0C << 0x08 ;  Execute-Only, conforming
+DESC_TYPE_CODE_EXCA     equ         0x0D << 0x08 ;  Execute-Only, conforming, accessed
+DESC_TYPE_CODE_EXRDC    equ         0x0E << 0x08 ;  Execute/Read, conforming
+DESC_TYPE_CODE_EXRDCA   equ         0x0F << 0x08 ;  Execute/Read, conforming, accessed
+
+
+DESC_S_SYS              equ         (0) << 0xc
+DESC_S_USR              equ         (1) << 0xc
+
+DESC_DPL_0              equ         (00b) << 0xd
+DESC_DPL_1              equ         (01b) << 0xd
+DESC_DPL_2              equ         (10b) << 0xd
+DESC_DPL_3              equ         (11b) << 0xd
+
+DESC_P_CLS              equ         (0) << 0xf
+DESC_P_SET              equ         (1) << 0xf
+
+DESC_L_CLS              equ         (0) << 0x15
+DESC_L_SET              equ         (1) << 0x15
+
+DESC_DB_D               equ         (0) << 0x16
+DESC_DB_B               equ         (1) << 0x16
+
+DESC_G_1B               equ         (0) << 0x17
+DESC_G_4K               equ         (1) << 0x17
--- boot/start.s
+++ boot/start.s
@@ -1,13 +1,30 @@
-; Bootloader starts here
+%include "gdt_helper.s"
 
+; address
+BOOTLOADER_BASE     equ     0x7c00
+STACK_BASE          equ     0x7c00
+OSLOADER_BASE       equ     0x8000
+
+
+; memory map
+; 0x0500 - 0x7bff   : stack
+; 0x7c00 - 0x7dff   : bootloader, mbr
+; 0x7e00 - 0x8000   : initial gdt table
+; 0x8000 - 0x9000   : os loader 
+
+; 0x80000 - 0xFFFFF ; unusable
+
+
+
+; Bootloader starts here
 [BITS 16]
-section mbr vstart=0x7c00
+section mbr vstart=BOOTLOADER_BASE
     mov ax, cs
     mov ds, ax
     mov es, ax
     mov ss, ax
     mov fs, ax
-    mov sp, 0x7c00
+    mov sp, STACK_BASE
 
     mov ax, 0x600
     mov bx, 0x700
@@ -26,16 +43,39 @@ section mbr vstart=0x7c00
     mov bx, 0x02
     int 0x10
 
-    mov eax, 0x02
-    mov bx, 0x8000
-    mov cx, 1
-    call read_disk
+; load osloader
+; osloader is close to mbr
+    mov eax, 0x01
+    mov bx, OSLOADER_BASE
+    mov cx, 10 ; 10 is enough
+    ;call read_disk_realmode
+
+; open A20
+    in al, 0x92
+    or al, 0000_0010b
+    out 0x92, al
+
+; set GDT
+    lgdt [gdt_addr]
+
+; set PE bit
+    mov eax, cr0
+    or eax, 1b
+    mov cr0, eax
 
     jmp $
+    jmp dword (1 << 3):pmode_start
+
+
+[BITS 32]
+pmode_start:
+
+
+[BITS 16]
 ; eax : LBA 
 ; bx : buffer
 ; cx : sector count
-read_disk:
+read_disk_realmode:
     mov esi, eax
     mov di, cx
 
@@ -84,6 +124,42 @@ read_disk:
     add bx, 2
     loop .go_on_read
     ret
+
+; global descriptor 
+; a data descriptor and a code descriptor
+
+
+; flat 4G
+gdt_addr:
+
+GDT_BASE            dd      0x0
+                    dd      0x0
+
+CODE_DESC           dd      0x0000ffff
+                    dd      (0 << DESC_BASE_OFFSET2)+ \
+                            DESC_TYPE_CODE_EX + \
+                            DESC_S_SYS + \
+                            DESC_DPL_0 + \
+                            DESC_P_SET + \
+                            DESC_L_CLS + \
+                            DESC_G_4K + \
+                            (0 << DESC_BASE_OFFSET3)
+
+DATA_DESC           dd      0x0000ffff
+                    dd      (0 << DESC_BASE_OFFSET2)+ \
+                            DESC_TYPE_DATA_RDWR + \
+                            DESC_S_SYS + \
+                            DESC_DPL_0 + \
+                            DESC_P_SET + \
+                            DESC_L_CLS + \
+                            DESC_G_4K + \
+                            (0 << DESC_BASE_OFFSET3)
+
+GDT_SIZE            equ     $ - GDT_BASE
+GDT_LIMIT           dd      GDT_SIZE - 1
+
+gdt_ptr:            dw      GDT_LIMIT
+                    dd      gdt_addr
 
 
 
